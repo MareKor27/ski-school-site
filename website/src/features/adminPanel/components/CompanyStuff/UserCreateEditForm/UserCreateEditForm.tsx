81108febbacd3496a2082341c578e399131b6cc9
MareKor27 <marek.korzeniewski1996@gmail.com>
2025-04-09 22:03:24 +0200

    error handling createedit form

--- website/src/features/adminPanel/components/CompanyStuff/UserCreateEditForm/UserCreateEditForm.tsx
+++ website/src/features/adminPanel/components/CompanyStuff/UserCreateEditForm/UserCreateEditForm.tsx
@@ -1,42 +1,49 @@
 import { useParams } from "react-router-dom";
 import { useUserInputData } from "~/features/adminPanel/hooks/user/useUserInputData";
 import { useUsers } from "~/features/adminPanel/hooks/user/useUsers";
-import { handleOnSubmit } from "~/features/adminPanel/services/CompanyStuffServices";
-
+import useStyles from "~/hooks/useStyle";
+import style from "./UserCreateEditForm.module.scss";
+const S = useStyles(style);
 export function UserCreateEditForm() {
   const params = useParams();
   const userId = params.id ? Number(params.id) : null;
 
   const { fetchResponse } = useUsers();
-  const { userForm, handleInputChange } = useUserInputData(userId);
+  const { register, handleSubmit, errors, createOrUpdateUser, sending } =
+    useUserInputData(userId, fetchResponse);
 
   return (
-    <form
-      onSubmit={(event) => {
-        event.preventDefault();
-        handleOnSubmit(userId, userForm, fetchResponse);
-      }}
-    >
+    <form onSubmit={handleSubmit(createOrUpdateUser)}>
       <label htmlFor="name">Imie i Nazwisko:</label>
       <input
         type="text"
-        id="name"
-        name="name"
-        size={50}
-        onChange={handleInputChange}
-        value={userForm.name}
+        {...register("name", {
+          required: "Imie i nazwisko jest wymagane",
+          minLength: {
+            value: 6,
+            message: "Pole musi mieć conajmneij 6 znaków",
+          },
+        })}
       />
+      <div className={S(`errors`)}>{errors.name && errors.name.message}</div>
       <label htmlFor="email">Email:</label>
       <input
         type="text"
-        id="email"
-        name="email"
-        size={50}
-        onChange={handleInputChange}
-        value={userForm.email}
+        {...register("email", {
+          required: "Email jest wymagane",
+          minLength: {
+            value: 6,
+            message: "Pole musi mieć conajmneij 6 znaków",
+          },
+        })}
       />
+      <div className={S(`errors`)}>{errors.email && errors.email.message}</div>
 
-      <input type="submit" value={userId ? "Aktualizuj" : "Stwórz"} />
+      <input
+        type="submit"
+        value={sending ? "Przetwarzanie" : userId ? "Aktualizuj" : "Stwórz"}
+        disabled={sending}
+      />
     </form>
   );
 }
--- website/src/features/adminPanel/hooks/user/useUserInputData.tsx
+++ website/src/features/adminPanel/hooks/user/useUserInputData.tsx
@@ -1,50 +1,86 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { UserForm } from "../../api/type/user.dto";
-import { readUser } from "../../api/AdminUserApi";
+import { readUser, updateUser } from "../../api/AdminUserApi";
+import { useForm } from "react-hook-form";
+import { createUser } from "../../api/AdminAuthApi";
+import { useNavigate } from "react-router-dom";
+import { Paths } from "~/features/app/constants/Paths";
+import { mapApiErrorToFormErrors } from "~/features/authorization/services/ErrorServices";
 
-export const useUserInputData = (userId: number | null) => {
-  const [userForm, setUserForm] = useState<UserForm>({
-    name: "",
-    email: "",
+export const useUserInputData = (
+  userId: number | null,
+  fetchResponse: () => void
+) => {
+  const navigate = useNavigate();
+  const [sending, setSending] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+    setError,
+  } = useForm<UserForm>({
+    mode: "onSubmit",
+    defaultValues: {
+      name: "",
+      email: "",
+    },
   });
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setUserForm((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
-  };
+  const createOrUpdateUser = useCallback(
+    async (userForm: UserForm) => {
+      setSending(true);
+      let response;
+      try {
+        if (userId) {
+          response = await updateUser(userId, {
+            name: userForm.name,
+            email: userForm.email,
+          });
+        } else {
+          response = await createUser(userForm.name, userForm.email);
+        }
+      } catch (error: any) {
+        const mappedErrors = mapApiErrorToFormErrors<UserForm>(error, [
+          "name",
+          "email",
+        ]);
+        console.log(error);
+        const result = Object.values(mappedErrors);
+        result.forEach((element) => {
+          setError(element.field, { message: element.message });
+        });
+      }
+      setSending(false);
+      if (!response) return;
+
+      fetchResponse();
+      navigate(Paths.ADMIN.STAFF.INDEX.absolute);
+    },
+    [userId]
+  );
 
   const fetchUser = async (userId: number) => {
     const response = await readUser(userId);
-    setUserForm({
-      name: response.content.name,
-      email: response.content.email,
-    });
-  };
-
-  const handleShowEditUser = (user: UserForm) => {
-    setUserForm({
-      name: user.name,
-      email: user.email,
-    });
+    setValue("name", response.content.name);
+    setValue("email", response.content.email);
   };
 
   useEffect(() => {
     if (userId) {
       fetchUser(userId);
     } else {
-      setUserForm({
-        name: "",
-        email: "",
-      });
+      setValue("name", "");
+      setValue("email", "");
     }
   }, [userId]);
 
   return {
-    userForm,
-    handleInputChange,
-    handleShowEditUser,
+    errors,
+    register,
+    handleSubmit,
+    createOrUpdateUser,
+    sending,
   } as const;
 };
