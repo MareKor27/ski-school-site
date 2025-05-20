d15fcda5c2601116deed539fd6a686da18e7dd00
MareKor27 <marek.korzeniewski1996@gmail.com>
2025-05-20 13:59:17 +0200

    Add logoutTimer to Zustand

--- website/src/features/authorization/store/useSessionStore.tsx
+++ website/src/features/authorization/store/useSessionStore.tsx
@@ -19,11 +19,10 @@ export type AuthState = {
   token: string | null;
   user: UserData | null;
   expirationDate: Date | null;
+  logoutTimer: ReturnType<typeof setTimeout> | null;
   refreshSession: (token: string, user: UserData, expirationDate: Date) => void;
   clear: () => void;
 };
-
-let logoutTimer: ReturnType<typeof setTimeout> | null = null;
 
 const serializeSesion = (
   token: string,
@@ -32,36 +31,55 @@ const serializeSesion = (
 ) => {
   localStorage.setItem("token", token);
   localStorage.setItem("user", JSON.stringify(user));
-  localStorage.setItem("expirationDate", JSON.stringify(expirationDate));
+  localStorage.setItem("expirationDate", expirationDate.toISOString());
 };
 
 export const useSessionStore = create<AuthState>((set, get) => ({
   token: localStorage.getItem("token"),
   user: JSON.parse(localStorage.getItem("user")!),
-  expirationDate: JSON.parse(localStorage.getItem("expirationDate")!),
+  expirationDate: localStorage.getItem("expirationDate")
+    ? new Date(localStorage.getItem("expirationDate")!)
+    : null,
+  logoutTimer: null,
 
   refreshSession: (token: string, user: UserData, expirationDate: Date) => {
     serializeSesion(token, user, expirationDate);
     set({ token, user, expirationDate });
 
-    if (logoutTimer) clearTimeout(logoutTimer);
+    const existingTimer = get().logoutTimer;
+    if (existingTimer) clearTimeout(existingTimer);
 
     const now = new Date().getTime();
     const expiration = new Date(expirationDate).getTime() - 30000;
     const timeout = expiration - now;
 
-    logoutTimer = setTimeout(async () => {
-      //If we want the user to have a session a certain amount of time
-      //useSessionStore.getState().clear();
+    // logoutTimer = setTimeout(async () => {
+    //   //If we want the user to have a session a certain amount of time
+    //   //useSessionStore.getState().clear();
 
-      //If we want the user  to have a session all the time and log out only at their request
-      const response = await extendSesionTime(user);
-      get().refreshSession(
-        response.accessToken,
-        response.user,
-        new Date(response.expirationDate)
-      );
+    //   //If we want the user  to have a session all the time and log out only at their request
+    //   const response = await extendSesionTime(user);
+    //   get().refreshSession(
+    //     response.accessToken,
+    //     response.user,
+    //     new Date(response.expirationDate)
+    //   );
+    // }, timeout);
+
+    const timer = setTimeout(async () => {
+      try {
+        const response = await extendSesionTime(user);
+        get().refreshSession(
+          response.accessToken,
+          response.user,
+          new Date(response.expirationDate)
+        );
+      } catch (error) {
+        get().clear();
+      }
     }, timeout);
+
+    set({ logoutTimer: timer });
   },
 
   clear: () => {
@@ -70,6 +88,8 @@ export const useSessionStore = create<AuthState>((set, get) => ({
     localStorage.removeItem("user");
     localStorage.removeItem("expirationDate");
 
-    if (logoutTimer) clearTimeout(logoutTimer);
+    const existingTimer = get().logoutTimer;
+    if (existingTimer) clearTimeout(existingTimer);
+    set({ logoutTimer: null });
   },
 }));
